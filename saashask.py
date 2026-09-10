a59fb735de1bbd36e3be65bd1e35f5ecbd48eaaa
June Margareth B. Ferraren <jmbajamunde@zrc.pshs.edu.ph>
2026-09-10 14:36:17 +0800

    Add files via upload

---
+++ saashask.py
@@ -0,0 +1,16 @@
+class Hero:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+    def take_damage(self, amount):
+        self.hp -= amount
+
+
+arthur = Hero("Arthur", 100)
+morgana = Hero("Morgana", 100)
+
+arthur.take_damage(10)
+
+print("Arthur:", arthur.hp)
+print("Morgan:", morgana.hp)
